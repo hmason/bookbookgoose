cacc9c323872529dafdc4f1f0549c4f598d05635
Hilary Mason <h@bit.ly>
2013-01-10 17:34:26 -0500

    no longer debug

--- app.py
+++ app.py
@@ -27,5 +27,5 @@ def api_get_books():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.debug = True # DEBUG
+    app.debug = False
     app.run(host='0.0.0.0', port=port)
